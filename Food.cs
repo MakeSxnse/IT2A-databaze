f54079c068d8f740a1b716a33f0858a0f1d17c9b
Václav Rolník <61820Rolnik@student.sstebrno.cz>
2026-03-26 13:24:49 +0100

    Inicializace

---
+++ Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaze
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int ConsumedPercent { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace databaze
+{
+
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Food> Foods { get; set; } = new ObservableCollection<Food>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Foods.Add(new Food { Name = "Jablko", Type = "Ovoce", Quantity = 3, ConsumedPercent = 50 });
+            Foods.Add(new Food { Name = "Chleba", Type = "Pečivo", Quantity = 1, ConsumedPercent = 20 });
+            Foods.Add(new Food { Name = "Mléko", Type = "Mléčný výrobek", Quantity = 2, ConsumedPercent = 80 });
+
+            DataContext = this;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Foods.Add(new Food
+            {
+                Name = tbName.Text,
+                Type = tbType.Text,
+                Quantity = int.Parse(tbQuantity.Text),
+                ConsumedPercent = int.Parse(tbConsumedPercent.Text)
+            });
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgJidlo.SelectedItem is Food f)
+            {
+                tbName.Text = f.Name;
+                tbType.Text = f.Type;
+                tbQuantity.Text = f.Quantity.ToString();
+                tbConsumedPercent.Text = f.ConsumedPercent.ToString();
+            }
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgJidlo.SelectedItem is Food f)
+            {
+                f.Name = tbName.Text;
+                f.Type = tbType.Text;
+                f.Quantity = int.Parse(tbQuantity.Text);
+                f.ConsumedPercent = int.Parse(tbConsumedPercent.Text);
+                dgJidlo.Items.Refresh();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgJidlo.SelectedItem is Food f)
+                Foods.Remove(f);
+        }
+
+        private void dgJidlo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
